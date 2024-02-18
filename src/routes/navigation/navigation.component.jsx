446636f42053964de190d98989caf2bdcb1ac56c
ghoozst01 <ghoozst01@gmail.com>
2024-02-17 21:07:11 -0600

    React router dom was added and also the nav bar was created

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,22 @@
+import { Fragment } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+import { ReactComponent as CrwLogo } from '../../assets/crown.svg';
+import './navigation.styles.scss';
+const Navigation = () => (
+  <Fragment>
+    <div className="navigation">
+      <Link className="logo-container" to="/">
+        <CrwLogo />
+      </Link>
+      <div className="nav-links-containers">
+        <Link className="nav-link" to="/shop">
+          Shop
+        </Link>
+      </div>
+    </div>
+    <Outlet />
+  </Fragment>
+);
+
+export default Navigation;
