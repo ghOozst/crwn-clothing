733bbb1f1434da5406b937790510e70fb6bcd0d5
ghoozst01 <ghoozst01@gmail.com>
2024-02-29 22:07:03 -0600

    now items can be added to the cart

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,13 +1,35 @@
 import { createContext, useState } from 'react';
 
+const addCartItem = (cartItems, productToAdd) => {
+  const foundItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+
+  if (!foundItem) return [...cartItems, { ...productToAdd, quantity: 1 }];
+
+  return cartItems.map((cartItem) =>
+    cartItem.id === productToAdd.id
+      ? { ...cartItem, quantity: cartItem.quantity + 1 }
+      : cartItem
+  );
+};
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const values = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+
+  const values = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
 
   return <CartContext.Provider value={values}>{children}</CartContext.Provider>;
 };
