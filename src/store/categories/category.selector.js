3f06eabe42c64a6207638507e4fc3d6b85c30dfd
ghoozst01 <ghoozst01@gmail.com>
2024-03-11 08:04:45 -0600

    correct a few mistypings in the categories selector

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,6 @@
 export const selectCategoriesMap = (state) =>
-  state.categories.categoriesMap.reduce((acc, category) => {
-    const { title, items } = category.data();
+  state.categories.categories.reduce((acc, category) => {
+    const { title, items } = category;
     acc[title.toLowerCase()] = items;
 
     return acc;
